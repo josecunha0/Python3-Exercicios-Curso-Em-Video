d4bc17bec9e49f56b2c9e1bd9208e59a42400fa7
José Fernando <88503510+josecunha0@users.noreply.github.com>
2022-05-27 11:13:16 -0300

    Create #003.py

---
+++ #003.py
@@ -0,0 +1,7 @@
+'''
+Crie um programa que leia dois números e mostre a soma entre eles.
+'''
+num1 = input("Por favor, digite o primeiro número: ")
+num2 = input("Agora, digite o segundo número: ")
+soma = num1 + num2
+print("A soma entre {} e {}, é {}.".format(num1, num2, soma))
