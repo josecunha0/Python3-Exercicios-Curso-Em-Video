1393898943fd6cd78ee2a61beb675757092fb8d7
José Fernando <88503510+josecunha0@users.noreply.github.com>
2022-05-28 14:52:17 -0300

    Update #014.py

--- #014.py
+++ #014.py
@@ -1,3 +1,7 @@
  '''
- 
+ Escreva um programa que converta uma temperatura digita em °C para °F.
  '''
+
+c = float(input("Digite a temperatura em °C: "))
+f = ((9*c)/5)+32
+print(f"A temperatura de {c:.1f}°C convertida para Farenheit, fica {f:.1f}°F!")
