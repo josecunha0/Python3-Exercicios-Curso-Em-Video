9e605ed8ef410af4e5a22cc69e0246f3b049f754
José Fernando <88503510+josecunha0@users.noreply.github.com>
2022-05-28 14:47:40 -0300

    Update #013.py

--- #013.py
+++ #013.py
@@ -1,3 +1,7 @@
  '''
- 
+ Faça um algotitmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
  '''
+
+s = float(input("Qual é o salário do funcionário? R$"))
+a = s + (s * 15/100)
+print(f"O salário que antes era R${s:.2f}, com 15% de aumento vai passar a ser R${a:.2f}!")
