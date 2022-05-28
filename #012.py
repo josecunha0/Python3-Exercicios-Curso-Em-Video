55645349f723041358ecf2c6bd90332d2f412b5a
José Fernando <88503510+josecunha0@users.noreply.github.com>
2022-05-28 14:43:46 -0300

    Update #012.py

--- #012.py
+++ #012.py
@@ -2,3 +2,7 @@
  Faça um programa que leia o preço de um produto e mostre seu novo preço com 5% de desconto.
  '''
  
+p = float(input("Qual o preço do produto? R$"))
+desconto = p * (5/100)
+total = p - desconto
+print(f"O produto que custava R${p:.2f}, agora com o desconto de 5% passa a custar R${total:.2f}!")
