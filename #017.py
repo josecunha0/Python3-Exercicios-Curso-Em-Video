752c755fac0644347648a34012f49048e9b76976
José Fernando <88503510+josecunha0@users.noreply.github.com>
2022-05-28 18:51:49 -0300

    Update #017.py

--- #017.py
+++ #017.py
@@ -1,3 +1,8 @@
 '''
-
+Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo retãngulo, calcule e mostre o comprimento da hipotenusa.
 '''
+
+co = float(input("Digite o comprimento do cateto oposto: "))
+ca = float(input("Digite o comprimento do cateto adjacente: "))
+hi = (co ** 2 + ca ** 2) ** (1/2)
+print(f"O valor da hipotenusa é {hi:.1f}")
