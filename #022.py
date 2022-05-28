333955d2ef19e3f8f0e77ad60a84a599ea1ba24b
José Fernando <88503510+josecunha0@users.noreply.github.com>
2022-05-28 19:43:49 -0300

    Update #022.py

--- #022.py
+++ #022.py
@@ -1,3 +1,16 @@
 '''
-
+Crie um programa que leia o nome completo de uma pessoa e mostre:
+- O nome com todas as letras maiúsculas e minúsculas.
+- Quantas letras ao todo (sem considerar espaços)
+- Quantas letras tem o primeiro nome [Não irei fazer porque quero o programa para textos e não nomes]
 '''
+
+texto = str(input("Digite o seu texto: ")).strip()
+print("Analisando o seu texto...")
+print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
+print("Seu texto todo em maiúsculo é: \n{}".format(texto.upper()))
+print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
+print("Seu texto todo em minuscula é: \n{}".format(texto.lower()))
+print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
+print("Seu texto ao todo, tem {} letras.".format(len(texto) - texto.count(" ")))
+print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
