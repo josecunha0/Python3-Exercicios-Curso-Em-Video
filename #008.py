edd1aa818ab1e77daed62970f74fb127efedadcc
José Fernando <88503510+josecunha0@users.noreply.github.com>
2022-05-28 13:49:59 -0300

    Update #008.py

--- #008.py
+++ #008.py
@@ -1,3 +1,8 @@
  '''
- 
+ Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
  '''
+
+m = float(input("Digite o valor em metros: "))
+cm = m*100
+mm = m*1000
+print(f"{m:.1f}M é o equivalente a {cm:.1f}cm e {mm:.1f}mm!")
