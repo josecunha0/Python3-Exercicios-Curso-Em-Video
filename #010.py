5d68569f59b150a25570369569f3e1a3385cad67
José Fernando <88503510+josecunha0@users.noreply.github.com>
2022-05-28 14:29:08 -0300

    Update #010.py

--- #010.py
+++ #010.py
@@ -1,3 +1,7 @@
  '''
- 
+ Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar. 
  '''
+
+d = float(input("Quanto em real você possui? R$"))
+conv = d / 4.73
+print(f"Com R${d:.2f} na carteira, você consegue comprar ${conv:.2f}!")
