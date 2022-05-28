4783fc966b1831708d2fe1b5a9665d7a83f2f62f
José Fernando <88503510+josecunha0@users.noreply.github.com>
2022-05-28 19:01:16 -0300

    Update #018.py

--- #018.py
+++ #018.py
@@ -1,3 +1,15 @@
 '''
-
+Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo. 
 '''
+
+from math import radians, sin, cos, tan
+
+an = float(input("Digite o ângulo que você deseja calcular: "))
+
+seno = sin(radians(an))
+cosseno = cos(radians(an))
+tangente = tan(radians(an))
+
+print(f"O valor do SENO do ângulo {an}, é {seno:.2f}")
+print(f"O valor do COSSENO do ângulo {an}, é {cosseno:.2f}")
+print(f"O valor do TANGENTE do ângulo {an}, é {tangente:.2f}")
