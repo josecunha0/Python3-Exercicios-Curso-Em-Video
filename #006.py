c9264903171d39a3bc206ac6ea7453fc12f452b5
José Fernando <88503510+josecunha0@users.noreply.github.com>
2022-05-28 12:17:28 -0300

    Update #006.py

--- #006.py
+++ #006.py
@@ -1,3 +1,11 @@
 '''
-
+Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
 '''
+n = input("Digite um número: ")
+d = n*2
+t = n*3
+r = n**(1/2)
+print(f"Analisando o número {n}, podemos afirmar que:"\n)
+print(f"O dobro do valor é {d}.")
+print(f"O triplo do valor é {t}.")
+print(f"E sua raiz quadrada é {r}.")
