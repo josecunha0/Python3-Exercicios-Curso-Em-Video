0268320d43d3820176bebadc794cbdf237d93362
José Fernando <88503510+josecunha0@users.noreply.github.com>
2022-05-28 11:51:41 -0300

    Create #005.py

---
+++ #005.py
@@ -0,0 +1,7 @@
+'''
+Faça um programa que leia um número inteiro e mostre na tela o seu sucesso e seu antecessor.
+'''
+n = input("Digite um número: ")
+a = n - 1
+s = n + 1
+print(f"Analisando o número {n}, seu antecessor é {a} e seu sucessor é {s}.")
