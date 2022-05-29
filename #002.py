42cbad594e749c7b83a39ac53ee2d4981aebaa1d
José Fernando <88503510+josecunha0@users.noreply.github.com>
2022-05-29 12:06:39 -0300

    Update #002.py

--- #002.py
+++ #002.py
@@ -1,6 +1,4 @@
-'''
-Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas vindas
-'''
+# Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas vindas
 
 nome = input('Digite o seu nome: ')
 print("É um prazer conhecê-lo, {}!", .format(nome))
