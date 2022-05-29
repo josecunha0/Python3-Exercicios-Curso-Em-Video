d77c2fdf388e68298a104290bf9ce13ad4a4bb4e
José Fernando <88503510+josecunha0@users.noreply.github.com>
2022-05-29 12:42:06 -0300

    Update #011.py

--- #011.py
+++ #011.py
@@ -1,6 +1,5 @@
- '''
- Faça um programa que leia a largura e a altura de uma parede em metros, calcule a sua area e a quantidade de tinta necessária para pintá-la, sabendo que cada litro de tinta, pinta uma área de 2m².
- '''
+# Faça um programa que leia a largura e a altura de uma parede em metros, calcule a sua area e a quantidade de tinta necessária para pintá-la, sabendo que cada litro de tinta, pinta uma área de 2m².
+
 
 larg = float(input("Largura da parede em metros: "))
 alt = float(input("Altura da parede em metros: "))
