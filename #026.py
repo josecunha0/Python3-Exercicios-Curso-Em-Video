00fa130a8e50b352c0d203722648b4e209bf5c7c
José Fernando <88503510+josecunha0@users.noreply.github.com>
2022-05-29 11:04:58 -0300

    Update #026.py

--- #026.py
+++ #026.py
@@ -1,3 +1,16 @@
 '''
-
+Faça um programa que leia uma frase pelo teclado e mostre: 
+- Quantas vezes aparece a letra "a".
+- Em que posição ela aparece a primeira vez.
+- Em que posição ela aparece a última vez.
 '''
+
+texto = str(input("Digite seu texto: ")).upper().strip()
+
+print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
+print("Analisando seu texto...")
+print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
+print("A letra 'A' aparece {} vezes no texto.".format(texto.count('A')))
+print("A primeira letra 'A' aparece na posição {}".format(texto.find('A')+1))
+print("A última letra 'A' aparece na posição {}".format(texto.rfind('A')+1))
+print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
