09f297fe7da87bf2130382876f818831ebb6909d
José Fernando <88503510+josecunha0@users.noreply.github.com>
2022-05-29 12:06:59 -0300

    Update #003.py

--- #003.py
+++ #003.py
@@ -1,6 +1,5 @@
-'''
-Crie um programa que leia dois números e mostre a soma entre eles.
-'''
+# Crie um programa que leia dois números e mostre a soma entre eles.
+
 num1 = input("Por favor, digite o primeiro número: ")
 num2 = input("Agora, digite o segundo número: ")
 soma = num1 + num2
