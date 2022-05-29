ab46f24673d30ba841665a42aba6ad2e600ea526
José Fernando <88503510+josecunha0@users.noreply.github.com>
2022-05-29 16:02:43 -0300

    Update #033.py

--- #033.py
+++ #033.py
@@ -1,1 +1,19 @@
+# Faça um programa que leia três números e mostre qual é o maior e qual é o menor
 
+n1 = int(input("Digite o primeiro valor: "))
+n2 = int(input("Digite o segundo valor: "))
+n3 = int(input("Digite o terceiro valor: "))
+
+if n1 > n2 and n1 > n3:
+    print(f"O maior valor é {n1}")
+elif n2 > n1 and n2 > n3:
+    print(f"O maior valor é {n2}")
+elif n3 > n1 and n3 > n2:
+    print(f"O maior valor é {n3}")
+    
+if n1 < n2 and n1 < n3:
+    print(f"O menor valor é {n1}")
+elif n2 < n1 and n2 < n3:
+    print(f"O menor valor é {n2}")
+elif n3 < n1 and n3 < n2:
+    print(f"O menor valor é {n3}")
