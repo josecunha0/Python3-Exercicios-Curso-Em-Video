3b61fd7ee670d792dc2145b57075274144aa3f8e
José Fernando <88503510+josecunha0@users.noreply.github.com>
2022-05-29 13:00:04 -0300

    Update #027.py

--- #027.py
+++ #027.py
@@ -1,6 +1,4 @@
-'''
-Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente.
-'''
+# Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente.
 
 print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
 nome = str(input("Digite o seu nome completo: ")).strip()
