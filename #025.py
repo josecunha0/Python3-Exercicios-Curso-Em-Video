b20e3069a622c4586b4b6d40a892d6b5736942b4
José Fernando <88503510+josecunha0@users.noreply.github.com>
2022-05-29 12:58:14 -0300

    Update #025.py

--- #025.py
+++ #025.py
@@ -1,6 +1,4 @@
-'''
-Crie um programa que leia o nome de uma pessoa e diga se ela tem "SILVA" no nome.
-'''
+# Crie um programa que leia o nome de uma pessoa e diga se ela tem "SILVA" no nome.
 
 nome = str(input("Digite o seu nome aqui: ")).strip()
 print("Seu nome tem Silva? {}".format("SILVA" in nome.upper()))
