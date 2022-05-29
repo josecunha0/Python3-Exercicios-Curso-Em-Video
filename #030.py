56bc7ee6b5026969d4ecd714af2487b502d6bcfb
José Fernando <88503510+josecunha0@users.noreply.github.com>
2022-05-29 12:05:13 -0300

    Update #030.py

--- #030.py
+++ #030.py
@@ -1,3 +1,19 @@
-'''
+# Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR.
 
-'''
+from time import sleep
+
+print("-=-"*10)
+n = int(input("Digite um número: "))
+
+resultado = n % 2
+print("Processando...")
+sleep(1.5)
+
+if resultado == 0:
+    print("-=-"*10)
+    print(f"O número {n} é PAR.")
+    print("-=-"*10)
+else:
+    print("-=-"*10)
+    print(f"O número {n} é ÍMPAR.")
+    print("-=-"*10)
