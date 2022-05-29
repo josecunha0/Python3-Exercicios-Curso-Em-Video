989c686309798b6583982450807d355bac14707f
José Fernando <88503510+josecunha0@users.noreply.github.com>
2022-05-29 12:07:20 -0300

    Update #004.py

--- #004.py
+++ #004.py
@@ -1,6 +1,4 @@
-'''
-Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele.
-'''
+# Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele.
 
 a = input("Digite algo: ")
 print("O tipo primitivo do que digitou é ", type(a))
