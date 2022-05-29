57761268839cbc41f798c2b53e5c2d3e0071f930
José Fernando <88503510+josecunha0@users.noreply.github.com>
2022-05-29 11:16:09 -0300

    Update #027.py

--- #027.py
+++ #027.py
@@ -1,3 +1,13 @@
 '''
-
+Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente.
 '''
+
+print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+nome = str(input("Digite o seu nome completo: ")).strip()
+n = nome.split()
+print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+print("Seu primeiro nome é {}".format(n[0]))
+print("E seu último nome é {}".format(n[len(n)-1]))
+print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+print("É um prazer conhecê-lo {}!!".format(n[0]))
+print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
