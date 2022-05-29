f8cebb9c8a7006945156c6eb95cd07a76e330990
José Fernando <88503510+josecunha0@users.noreply.github.com>
2022-05-29 16:55:25 -0300

    Update #036.py

--- #036.py
+++ #036.py
@@ -1,4 +1,11 @@
 # Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa. Pergunte o valor da casa, o salário do comprador e em quantos anos ele vai pagar.
 # A prestação mensal, não pode exceder 30% do salário ou então o empréstimo será negado.
 
+#rascunho do código: 
 
+# casa = float(input("Digite o valor da casa: R$"))
+# salario = float(input("Digite o seu salário: R$"))
+# anos = int(input("Em quantos anos pretende pagar? "))
+
+# p = casa / (anos * 12)
+# print(f"Para pagar uma casa que custa R${casa:.2f} em {anos} anos, as prestações serão de R${p:.2f} sem juros.")
