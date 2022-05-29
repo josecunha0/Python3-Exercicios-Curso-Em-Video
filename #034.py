8f21823438c60f1c466fc4a7f0827f3de3e9f9de
José Fernando <88503510+josecunha0@users.noreply.github.com>
2022-05-29 16:26:54 -0300

    Update #034.py

--- #034.py
+++ #034.py
@@ -1,1 +1,13 @@
+# Escreva um programa que pergunte o salário de um funcionário e calcule o valor do seu aumento. 
+# Para salários superiores a R$1250.00, calcule um aumento de 10%.
+# Para os inferiores ou iguais, o aumento é de 15%.
 
+salario = float(input("Digite o salário do funcionário: R$"))
+
+a1 = salario * 15/100
+a2 = salario * 10/100
+
+if salario <= 1250:
+    print(f"Com o aumento de 15%, o novo salário passa a ser R${salario + a1:.2f}")
+else:
+    print(f"Com o aumento de 10%, o novo salário passa a ser R${salario + a2:.2f}")
