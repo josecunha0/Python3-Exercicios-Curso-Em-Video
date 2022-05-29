b631ecfc19b06b641847d10369f9ec6e8fcc90ed
José Fernando <88503510+josecunha0@users.noreply.github.com>
2022-05-29 12:53:49 -0300

    Update #022.py

--- #022.py
+++ #022.py
@@ -1,9 +1,7 @@
-'''
-Crie um programa que leia o nome completo de uma pessoa e mostre:
-- O nome com todas as letras maiúsculas e minúsculas.
-- Quantas letras ao todo (sem considerar espaços)
-- Quantas letras tem o primeiro nome [Não irei fazer porque quero o programa para textos e não nomes]
-'''
+# Crie um programa que leia o nome completo de uma pessoa e mostre:
+# - O nome com todas as letras maiúsculas e minúsculas.
+# - Quantas letras ao todo (sem considerar espaços)
+# - Quantas letras tem o primeiro nome [Não irei fazer porque quero o programa para textos e não nomes]
 
 texto = str(input("Digite o seu texto: ")).strip()
 print("Analisando o seu texto...")
