b71ffd86c6cbb14d6478a603d8d6c86a6e8525ac
José Fernando <88503510+josecunha0@users.noreply.github.com>
2022-05-29 12:52:28 -0300

    Update #020.py

--- #020.py
+++ #020.py
@@ -1,6 +1,4 @@
-'''
- O mesmo professor do desafio anterior quer sortear a ordem de apresentação de trabalhos dos alunos. Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
-'''
+# O mesmo professor do desafio anterior quer sortear a ordem de apresentação de trabalhos dos alunos. Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
 
 from random import shuffle
 
