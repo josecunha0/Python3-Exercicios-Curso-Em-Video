df8dffb9856ed2ca78e34fea52664252dbf3773f
José Fernando <88503510+josecunha0@users.noreply.github.com>
2022-05-29 12:08:44 -0300

    Update #007.py

--- #007.py
+++ #007.py
@@ -1,6 +1,4 @@
-'''
-Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
-'''
+# Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
 
 n1 = float(input("Digite a primeira nota: "))
 n2 = float(input("Digite a segunda nota: "))
