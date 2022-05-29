27786d608870faaeb92939183e65ce75ae6fe8aa
José Fernando <88503510+josecunha0@users.noreply.github.com>
2022-05-29 11:48:14 -0300

    Update #029.py

--- #029.py
+++ #029.py
@@ -1,3 +1,13 @@
-'''
+# Escreva um programa que leia a velocidade de um carro. Se ele ultrapassar 80km/h, mostre uma mensagem dizendo que ele foi multado. 
+# A multa vai custar R$7,00 por cada Km acima do limite. 
 
-'''
+v = float(input("Qual é a velocidade atual do carro? "))
+
+if v > 80:
+    print("-=-"*20)
+    print("Você recebeu uma multa por exceder o limite de 80Km/h.")
+    multa = (v-80)*7
+    print(f"Você terá que pagar uma multa no valor de R${multa:.2f}!")
+    
+print("-=-"*20)
+print("Tenha um bom dia! Dirija com segurança!")
