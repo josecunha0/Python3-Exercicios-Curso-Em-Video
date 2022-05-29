fb3d260668519c47a88b64172fb94536d0d48a0a
José Fernando <88503510+josecunha0@users.noreply.github.com>
2022-05-29 12:42:25 -0300

    Update #012.py

--- #012.py
+++ #012.py
@@ -1,7 +1,5 @@
- '''
- Faça um programa que leia o preço de um produto e mostre seu novo preço com 5% de desconto.
- '''
- 
+# Faça um programa que leia o preço de um produto e mostre seu novo preço com 5% de desconto.
+
 p = float(input("Qual o preço do produto? R$"))
 desconto = p * (5/100)
 total = p - desconto
