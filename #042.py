66aeef183a9c8d9360d40c7a60e82f16d7ea791f
José Fernando <88503510+josecunha0@users.noreply.github.com>
2022-06-07 14:43:18 -0300

    Update #042.py

--- #042.py
+++ #042.py
@@ -1,1 +1,19 @@
+# Refaça o desafio 35 dos triângulos, acrescentando o recurso de mostrar que tipo de triângulo será formado: 
+# Equilátero: todos os lados iguais 
+# Isósceles: dois lados iguais
+# Escaleno: todos os lados diferentes
 
+r1 = float(input("Digite o primeiro segmento: "))
+r2 = float(input("Digite o segundo segmento: ")) 
+r3 = float(input("Digite o terceiro segmento: "))
+
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print("Os segmentos acima PODEM FORMAR um triângulo ", end=" ")
+    if r1 == r2 == r3:
+        print("EQUILÁTERO!")
+    elif r1 != r2 != r3 != r1:
+        print("ESCALENO!")
+    else:
+        print("ISÓSCELES!")
+else:
+    print("Os segmentos acima NÃO PODEM FORMAR um triângulo.")
